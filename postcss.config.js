fd43db4d0fcf7284baf17d5576f11da59d09af82
ZhaoXiaodong <zhaoxd@zenmen.com>
2018-10-11 17:17:21 +0800

    chore: 关闭 px2rem

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,5 @@
-const px2rem = require('postcss-px2rem');
-
 module.exports = {
   plugins: [
-    require('autoprefixer'),
-    px2rem({
-      remUnit: 75
-    })
+    require('autoprefixer')
   ]
 };
